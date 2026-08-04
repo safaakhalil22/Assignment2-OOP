40cf2d175aab8540ca9d28234d2cbae310095c18
Safaa-khalil <safaa.khalil@msa.edu.eg>
2026-08-05 01:21:34 +0300

    InternationalShipment

--- structs/InternationalShipment.cs
+++ structs/InternationalShipment.cs
@@ -9,60 +9,60 @@ namespace Assignment2_OOP.structs
     internal class InternationalShipment : Shipment
     {
         #region InternationalShipment
-        #region Attrbiutes
-        private string? destinationCountry;
-        private decimal customsFee;
-        #endregion
+        //#region Attrbiutes
+        //private string? destinationCountry;
+        //private decimal customsFee;
+        //#endregion
 
-        #region Properties
-        public string? DestinationCountry
-        {
-            get { return destinationCountry; }
-            set { destinationCountry = string.IsNullOrWhiteSpace(value) ? destinationCountry : value; }
-        }
+        //#region Properties
+        //public string? DestinationCountry
+        //{
+        //    get { return destinationCountry; }
+        //    set { destinationCountry = string.IsNullOrWhiteSpace(value) ? destinationCountry : value; }
+        //}
 
-        public decimal CustomsFee
-        {
-            get { return customsFee; }
-            set { customsFee = value >= 0 ? value : customsFee; }
-        }
+        //public decimal CustomsFee
+        //{
+        //    get { return customsFee; }
+        //    set { customsFee = value >= 0 ? value : customsFee; }
+        //}
 
       
-        public override decimal EstimatedCost
-        {
-            get { return base.EstimatedCost + CustomsFee; }
-        }
-        #endregion
+        //public override decimal EstimatedCost
+        //{
+        //    get { return base.EstimatedCost + CustomsFee; }
+        //}
+        //#endregion
 
-        #region Constructors
+        //#region Constructors
        
-        public InternationalShipment(string? trackingCode, string? description, double weight,
-                                      decimal deliveryFee, DeliveryAddress destination,
-                                      string? destinationCountry, decimal customsFee)
-            : base(trackingCode, description, weight, deliveryFee, destination)
-        {
-            this.destinationCountry = null;
-            this.customsFee = 0;
+        //public InternationalShipment(string? trackingCode, string? description, double weight,
+        //                              decimal deliveryFee, DeliveryAddress destination,
+        //                              string? destinationCountry, decimal customsFee)
+        //    : base(trackingCode, description, weight, deliveryFee, destination)
+        //{
+        //    this.destinationCountry = null;
+        //    this.customsFee = 0;
 
-            DestinationCountry = destinationCountry;
-            CustomsFee = customsFee;
-        }
-        #endregion
+        //    DestinationCountry = destinationCountry;
+        //    CustomsFee = customsFee;
+        //}
+        //#endregion
 
-        #region Methods
-        public override void PrintShipment()
-        {
-            Console.WriteLine("International Shipment");
-            Console.WriteLine();
-            Console.WriteLine($"Tracking Code      : {TrackingCode}");
-            Console.WriteLine($"Description        : {Description}");
-            Console.WriteLine($"Weight             : {Weight} kg");
-            Console.WriteLine($"Delivery Fee       : {DeliveryFee}");
-            Console.WriteLine($"Destination Country: {DestinationCountry}");
-            Console.WriteLine($"Customs Fee        : {CustomsFee}");
-            Console.WriteLine($"Estimated Cost     : {EstimatedCost}");
-        }
-        #endregion
+        //#region Methods
+        //public override void PrintShipment()
+        //{
+        //    Console.WriteLine("International Shipment");
+        //    Console.WriteLine();
+        //    Console.WriteLine($"Tracking Code      : {TrackingCode}");
+        //    Console.WriteLine($"Description        : {Description}");
+        //    Console.WriteLine($"Weight             : {Weight} kg");
+        //    Console.WriteLine($"Delivery Fee       : {DeliveryFee}");
+        //    Console.WriteLine($"Destination Country: {DestinationCountry}");
+        //    Console.WriteLine($"Customs Fee        : {CustomsFee}");
+        //    Console.WriteLine($"Estimated Cost     : {EstimatedCost}");
+        //}
+        //#endregion
         #endregion
 
     }
