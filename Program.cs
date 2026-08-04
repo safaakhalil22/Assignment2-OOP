6a7200f59b6586f841e42ed04e055587d3209862
Safaa-khalil <safaa.khalil@msa.edu.eg>
2026-08-05 00:53:11 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment2_OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
