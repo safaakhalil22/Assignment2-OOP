adbe8aedef089b65560914f6afdfcec16676d800
Safaa-khalil <safaa.khalil@msa.edu.eg>
2026-08-05 01:25:12 +0300

    DeliveryAddress

--- structs/DeliveryAddress.cs
+++ structs/DeliveryAddress.cs
@@ -9,32 +9,32 @@ namespace Assignment2_OOP.structs
     public struct DeliveryAddress
     {
         #region DeliveryAddress
-        #region Attributes
-        public string? City;
-        public string? Street;
-        public int BuildingNumber;
-        #endregion
+        //#region Attributes
+        //public string? City;
+        //public string? Street;
+        //public int BuildingNumber;
+        //#endregion
 
-        #region Constructors
-        public DeliveryAddress(string? city, string? street, int buildingNumber)
-        {
-            City = city;
-            Street = street;
-            BuildingNumber = buildingNumber;
-        }
-        #endregion
+        //#region Constructors
+        //public DeliveryAddress(string? city, string? street, int buildingNumber)
+        //{
+        //    City = city;
+        //    Street = street;
+        //    BuildingNumber = buildingNumber;
+        //}
+        //#endregion
 
-        #region Methods
-        public string GetFullAddress()
-        {
-            return $"{BuildingNumber} {Street}, {City}";
-        }
+        //#region Methods
+        //public string GetFullAddress()
+        //{
+        //    return $"{BuildingNumber} {Street}, {City}";
+        //}
 
-        public override string ToString()
-        {
-            return GetFullAddress();
-        }
-        #endregion
+        //public override string ToString()
+        //{
+        //    return GetFullAddress();
+        //}
+        //#endregion
         #endregion
     }
 }
