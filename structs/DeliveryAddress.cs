02cb717f969f4a7863791d92b878f008a7bad208
Safaa-khalil <safaa.khalil@msa.edu.eg>
2026-08-05 01:21:05 +0300

    DeliveryCenter

---
+++ structs/DeliveryAddress.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2_OOP.structs
+{
+    public struct DeliveryAddress
+    {
+        #region DeliveryAddress
+        #region Attributes
+        public string? City;
+        public string? Street;
+        public int BuildingNumber;
+        #endregion
+
+        #region Constructors
+        public DeliveryAddress(string? city, string? street, int buildingNumber)
+        {
+            City = city;
+            Street = street;
+            BuildingNumber = buildingNumber;
+        }
+        #endregion
+
+        #region Methods
+        public string GetFullAddress()
+        {
+            return $"{BuildingNumber} {Street}, {City}";
+        }
+
+        public override string ToString()
+        {
+            return GetFullAddress();
+        }
+        #endregion
+        #endregion
+    }
+}
---
+++ structs/StandardShipment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2_OOP.structs
+{
+    internal class StandardShipment : Shipment
+    {
+        #region Constructors
+       
+        public StandardShipment(string? trackingCode, string? description, double weight,
+                                 decimal deliveryFee, DeliveryAddress destination)
+            : base(trackingCode, description, weight, deliveryFee, destination)
+        {
+        }
+        #endregion
+    }
+}
